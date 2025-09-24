f58e94c60624ba47dbbc2f0b058e0973613c07b2
Elara <gwenlynn.lim@gmail.com>
2025-09-24 15:12:08 +0800

    Anewer q3.py

--- src/q3.py
+++ src/q3.py
@@ -1,10 +1,7 @@
 def update_dictionary(dct, key, value):
-    """
-    Task 1
-    - Create a function that updates a dictionary (dct) with a new key-value pair.
-    - If the key already exists in dct, print the original value, then update its value.
-    - Return the updated dictionary.
-    """
+
+# Task 1
+
     # Check if the key already exists in the dictionary.
     if key in dct:
         # If the key exists, print the original value.
