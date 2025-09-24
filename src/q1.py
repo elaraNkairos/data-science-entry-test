f31b6360506d8243932e772d053da4ce1dbdd154
Elara <gwenlynn.lim@gmail.com>
2025-09-24 15:10:33 +0800

    answer q1.py

--- src/q1.py
+++ src/q1.py
@@ -1,12 +1,7 @@
 def swap(x, y):
-    """
+
     # Task 1
-    - Create a function that would swap the value of x and y using only x and y as variables.
-    - x and y must be numeric.
-    - Return -1 if x and y is not numeric, and
-    - print the swapped values if both x and y are numeric.
-    """
-    
+   
     # Check if both x and y are numeric
     if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
         print("Error: x and y must be numeric.")
