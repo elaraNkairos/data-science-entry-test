d3fcda126efae049c06ef66d397e578c27b727b0
Elara <gwenlynn.lim@gmail.com>
2025-09-24 15:11:20 +0800

    Answer q2.py

--- src/q2.py
+++ src/q2.py
@@ -1,10 +1,6 @@
 def find_and_replace(lst, find_val, replace_val):
-    """
-    Task 1
-    - Create a function that searches for all occurrences of a value (find_val) in a given list (lst) and replaces them with another value (replace_val).
-    - lst must be a list.
-    - Return the modified list.
-    """
+    # Task 1
+
     # Check if the first argument is a list.
     if not isinstance(lst, list):
         print("Error: The first argument must be a list.")
